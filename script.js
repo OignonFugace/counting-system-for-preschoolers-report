60b687c21ef7629f7cd6dcfb8e03a2e623e61634
Tanguy <tanguy.freycon@gmail.com>
2023-01-27 15:13:05 +0100

    Try adding scrollspy feature with simple-scrollspy library. Not working yet

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+import scrollSpy from 'simple-scrollspy';
 
+/* ANCHORS */
 anchors.options = {
     placement: 'left',
     icon: '#'
 };
 
 anchors.add();
+
+
+/* SCROLLSPY */
+const menuElement = document.querySelector('#toc');
+console.log(menuElement);
+scrollSpy(menuElement);
