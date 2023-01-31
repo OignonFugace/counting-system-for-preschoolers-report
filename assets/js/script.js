067e66ab596791fb04ddd92bf7df68845c457487
Tanguy <tanguy.freycon@gmail.com>
2023-01-31 06:43:19 +0100

    Remove nav toc hover and focuss effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,12 +93,3 @@ window.addEventListener('DOMContentLoaded', () => {
   })
 })()
 
-
-/* DARK THEME CUSTOM STYLES */
-
-// window.addEventListener('DOMContentLoaded', () => {
-//   const nav = document.querySelector("#toc");
-//   const navList = nav.firstElementChild;
-//   console.log(nav);
-//   console.log(navList);
-// });
